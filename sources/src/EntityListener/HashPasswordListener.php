2071cdde3b8b048014f67f0fbed58444a9e25784
Mark1Z <muslim1992@gmail.com>
2020-08-05 10:36:07 +0300

    Update HashPasswordListener.php

--- sources/src/EntityListener/HashPasswordListener.php
+++ sources/src/EntityListener/HashPasswordListener.php
@@ -4,46 +4,63 @@ namespace App\EntityListener;
 
 use App\Entity\User;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as UserPasswordEncoder;
 
+/**
+ * Class HashPasswordListener
+ *
+ * @package App\EntityListener
+ */
 class HashPasswordListener
 {
+    /**
+     * @var UserPasswordEncoder
+     */
     private $passwordEncoder;
 
+    /**
+     * HashPasswordListener constructor.
+     *
+     * @param UserPasswordEncoder $passwordEncoder
+     */
     public function __construct(UserPasswordEncoder $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
     }
 
+    /**
+     * @param User $user
+     * @param LifecycleEventArgs $args
+     */
     public function prePersist(User $user, LifecycleEventArgs $args)
     {
-        if (!$user->getPlainPassword()) {
-            return;
-        }
         $this->encodePassword($user);
     }
 
+    /**
+     * @param User $user
+     * @param PreUpdateEventArgs $args
+     */
+    public function preUpdate(User $user, PreUpdateEventArgs $args)
+    {
+        $this->encodePassword($user);
+    }
+
+    /**
+     * @param User $entity
+     */
     private function encodePassword(User $entity)
     {
         if (!$entity->getPlainPassword()) {
             return;
         }
+
         $encoded = $this->passwordEncoder->encodePassword(
             $entity,
             $entity->getPlainPassword()
         );
+
         $entity->setPassword($encoded);
-    }
-
-    public function preUpdate(User $user, LifecycleEventArgs $args)
-    {
-        if (!$user->getPlainPassword()) {
-            return;
-        }
-        $this->encodePassword($user);
-
-        $em = $args->getEntityManager();
-        $em->getUnitOfWork()->persist($user);
-        $em->getUnitOfWork()->commit($user);
     }
 }
